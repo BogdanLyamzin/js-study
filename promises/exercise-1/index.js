2116e15642d8d0363374b4e08043ef894b92b27c
BogdanLyamzin <bogdan.lyamzin.d@gmail.com>
2025-04-13 19:09:25 +0200

    add promise example & exercises

---
+++ promises/exercise-1/index.js
@@ -0,0 +1,17 @@
+/*
+Створіть 2 проміси:
+ - перший через 2 секунди успішно завершується, записуючи в result масив книг нижче;
+ - другий за 1 секунду завершується з помилкою з текстом "Not found";
+*/
+const books = [
+    {
+        "id": "u9kgwNWGi3uUUwh0b8V49",
+        "title": "Eloquent JavaScript, Third Edition",
+        "author": "Marijn Haverbeke"
+    },
+    {
+        "id": "YxhM4QDxPeA3SmPHcEZPJ",
+        "title": "Practical Modern JavaScript",
+        "author": "Nicolas Bevacqua"
+    },
+];
---
+++ promises/exercise-2/index.js
@@ -0,0 +1,16 @@
+/*
+Напишіть проміс, який успішно виконується через 2 секунди та повертає масив книг нижче. 
+Після чого візьміть цей масив, зробіть із нього масив тегів li та виведіть у ul з id="book-list"
+*/
+const books = [
+  {
+    id: "u9kgwNWGi3uUUwh0b8V49",
+    title: "Eloquent JavaScript, Third Edition",
+    author: "Marijn Haverbeke",
+  },
+  {
+    id: "YxhM4QDxPeA3SmPHcEZPJ",
+    title: "Practical Modern JavaScript",
+    author: "Nicolas Bevacqua",
+  },
+];
